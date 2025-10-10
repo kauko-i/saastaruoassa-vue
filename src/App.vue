1572eb31149f1b4673ba6d8d3dc442143eec546a
Ilari Kauko <ilarikauko@gmail.com>
2025-10-10 17:24:42 +0300

    use component files

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const suodatuksessa = ref(false);
+
+function switchSuodata(e) {
+  e.preventDefault();
+  console.log(suodatuksessa.value);
+  suodatuksessa.value = !suodatuksessa.value;
+}
+
+onMounted(() => {
+  console.log(`Alkuperäinen suodatuksessa on ${suodatuksessa.value}.`)
+})
+
+</script>
+
+<template>
+  <meta charset="UTF-8">
+    <h1>Laske, kuinka täytät ravitsemussuositukset halvimmalla!</h1>
+    Laskuri perustuu:
+    <ul>
+      <li>50 ruoka-aineen <a href="https://fineli.fi/fineli/fi/index">ravintoarvoihin</a>,</li>
+      <li>niiden keskimääräisiin <a href="https://www.s-kaupat.fi/">hintoihin Prismoissa</a>,</li>
+      <li>valtion ravitsemusneuvottelukunnan <a href="">suosituksiin</a> vitamiinien, kivennäisaineiden, proteiinien ja rasvojen saannista ja</li>
+      <li><a href="https://en.wikipedia.org/wiki/Linear_programming">lineaariseen optimointiin</a>.</li>
+    </ul>
+    Syötä tietosi:
+    <form>
+      <fieldset>
+        <label>Ikä:
+          <select>
+            <option>2-3</option>
+            <option>4-6</option>
+            <option>7-10</option>
+            <option>11-14</option>
+            <option>15-17</option>
+            <option>18-24</option>
+            <option>25-50</option>
+            <option>51-69</option>
+            <option>70-74</option>
+            <option>>75</option>
+          </select>
+        </label>
+        <label> Sukupuoli: 
+          <select>
+            <option>Mies</option>
+            <option>Nainen</option>
+          </select>
+        </label>
+        <label> Energiantarve (kcal/päivä): <input type="number"></label>
+      </fieldset>
+      <fieldset>
+        <label><input type="checkbox">Keliakia</label>
+        <label><input type="checkbox">Laktoosi-intoleranssi</label>
+        <label><input type="checkbox">Vegetaristi</label>
+        <label><input type="checkbox">Vegaani</label>
+      </fieldset>
+      <button @click="switchSuodata">{{ suodatuksessa ? 'Älä suodatakaan' : 'Suodata ruokia manuaalisesti' }}</button>
+    </form>
+</template>
---
+++ src/Yksittaiset.js
@@ -0,0 +1,9 @@
+export default {
+  template: `<template>
+    <label><input type="checkbox" checked>Appelsiini</label>
+    <label><input type="checkbox" checked>Banaani</label>
+    <label><input type="checkbox" checked>Broileri</label>
+    <label><input type="checkbox" checked>Edamjuusto</label>
+    <label><input type="checkbox" checked>Emmentaljuusto</label>
+  </template>`
+}
